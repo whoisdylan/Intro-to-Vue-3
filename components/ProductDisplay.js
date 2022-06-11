fa5bb35ceb87e7b5f04b491a83da89950ab11a31
Dylan Koenig <koenigup@gmail.com>
2022-06-10 19:25:25 -0700

    complete L10

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -38,6 +38,13 @@ app.component('product-display', {
           v-on:click="addToCart">
           Add to Cart
         </button>
+        <button 
+          class="button" 
+          :class="{ disabledButton: !inStock }" 
+          :disabled="!inStock" 
+          v-on:click="removeFromCart">
+          Remove from Cart
+        </button>
       </div>
     </div>
     <review-list v-if="reviews.length" :reviews="reviews"></review-list>
@@ -59,6 +66,9 @@ app.component('product-display', {
   methods: {
       addToCart() {
           this.$emit('add-to-cart', this.variants[this.selectedVariant].id)
+      },
+      removeFromCart() {
+          this.$emit('remove-from-cart', this.variants[this.selectedVariant].id)
       },
       updateVariant(index) {
           this.selectedVariant = index
--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
+            cart: [],
             premium: true
         }
     },
-    methods: {}
+    methods: {
+      updateCart(id) {
+        this.cart.push(id)
+      },
+      removeFromCart(id) {
+        for (let i = 0; i < this.cart.length; i++) {
+          if (this.cart[i] === id) {
+            console.log("CountDebugula 4");
+            this.cart.splice(i,1)
+            return
+          }
+        }
+      }
+    }
 })
